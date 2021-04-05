056361b69d7618bf2f2f65f950f659cbccc94a57
Jack Phoenix <ashley@uncyclomedia.co>
2021-04-05 11:49:02 +0300

    Ditch the Skin::setupSkinUserCss method(), require MW 1.35+
    Bug: T266735
    Change-Id: Id3c3f5fe443641e366e0bf947bb3faa195c344da

--- includes/SkinDusk.php
+++ includes/SkinDusk.php
@@ -8,15 +8,4 @@
 class SkinDusk extends SkinTemplate {
 	public $skinname = 'dusk', $stylename = 'dusk',
 		$template = 'DuskTemplate';
-
-	function setupSkinUserCss( OutputPage $out ) {
-		parent::setupSkinUserCss( $out );
-
-		// Add CSS via ResourceLoader
-		$out->addModuleStyles( array(
-			'mediawiki.skinning.interface',
-			'mediawiki.skinning.content.externallinks',
-			'skins.dusk'
-		) );
-	}
 }
