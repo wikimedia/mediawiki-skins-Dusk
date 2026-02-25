fb073cce101fa07a4a099e5dbef08e9a30d290a5
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2026-02-25 16:21:58 +0000

    build: Updating mediawiki/mediawiki-phan-config to 0.19.0
    Change-Id: I61395be46644dd6c6a7c655bd40b83f633048068

--- includes/DuskTemplate.php
+++ includes/DuskTemplate.php
@@ -117,7 +117,6 @@ class DuskTemplate extends BaseTemplate {
 	 */
 	protected function renderPortals( $sidebar ) {
 		if ( !isset( $sidebar['SEARCH'] ) ) {
-			// @phan-suppress-next-line PhanTypeMismatchDimAssignment
 			$sidebar['SEARCH'] = true;
 		}
 		if ( !isset( $sidebar['TOOLBOX'] ) ) {
